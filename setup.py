899c6d298d0d397a9c1d774f2d7a486fcdf9db71
jutanke <jutanke@googlemail.com>
2019-04-16 09:26:34 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="mocap",
+    version="0.0.1",
+    packages=["mocap",
+              "mocap/data",
+              "mocap/visualization"
+              ],
+)
