6ff336e8215f77bf349010975d7c1846c89808ae
jutanke <jutanke@googlemail.com>
2019-04-16 11:16:10 +0200

    add normalization step

---
+++ mocap/math/geometry.py
@@ -0,0 +1,190 @@
+import numpy as np
+import numba as nb
+import numpy.linalg as la
+from math import pi
+
+
+# === 2 dimensions ===
+
+
+@nb.jit(nb.float32[:, :](
+    nb.float32
+), nopython=True, nogil=True )
+def rot2d(alpha):
+    """ get a 2d rotation matrix
+    :param alpha: in radians
+    :returns rotation matrix
+    """
+    R = np.array([
+        [np.cos(alpha), -np.sin(alpha)],
+        [np.sin(alpha), np.cos(alpha)]
+    ], np.float32)
+    return R
+
+
+@nb.jit(nb.float32[:, ](
+    nb.float32[:, ], nb.float32[:, ]
+), nopython=True, nogil=True)
+def get_2d_normal(left, right):
+    """ gets the 2d normal
+    :param left: [x, y]
+    :param right: [x, y]
+    """
+    lr = left - right
+    lr = lr/la.norm(lr)
+    R = rot2d(-pi/2)
+    n = R @ lr
+    return n
+
+
+@nb.jit(nb.float32(
+    nb.float32[:, ]
+), nopython=True, nogil=True)
+def get_2d_rotation_for_upward(p):
+    """ gets alpha to rotate point p to [0, 1]
+    :param p: [x, y]
+    :return angle in radians
+    """
+    p = p / la.norm(p)  # normalize (just to be sure)
+    up = np.array([0, 1], np.float32)
+
+    alpha = np.arccos(up @ p)
+    if p[0] < 0:
+        alpha *= -1
+
+    return alpha
+
+# === 3 dimensions ===
+
+
+def distances(human1, human2):
+    """ calculate distances between two humans for each joint
+    :param human1: [ (x, y, z), ... ]
+    :param human2: [ (x, y, z), ... ]
+    """
+    J = len(human1)
+    assert len(human2) == J
+    return _distances(human1, human2)
+
+
+@nb.jit(nb.float32[:, ](
+    nb.float32[:, :], nb.float32[:, :]
+), nopython=True, nogil=True)
+def _distances(human1, human2):
+    """ calculate distances between two humans for each joint
+    :param human1: [ (x, y, z), ... ]
+    :param human2: [ (x, y, z), ... ]
+    """
+    J = len(human1)
+    results = np.empty((J, ), np.float32)
+    for jid in range(J):
+        a = human1[jid]
+        b = human2[jid]
+        results[jid] = la.norm(a - b)
+
+    return results
+
+
+@nb.jit(nb.float32[:, ](
+    nb.float32[:, ], nb.float32[:, ], nb.int32[:, ]
+), nopython=True, nogil=True)
+def get_3d_normal_on_plane(left, right, plane):
+    """ calculate 3d normal on defined plane
+    :param left: (x, y, z)
+    :param right: (x, y, z)
+    :return
+    """
+    oo_plane_dim = -1  # find out-of-plane dimension
+    for i in range(3):
+        i_is_not_in_plane = True
+        for j in plane:
+            if i == j:
+                i_is_not_in_plane = False
+
+        if i_is_not_in_plane:
+            oo_plane_dim = i
+            break
+
+    oo_mean = (left[oo_plane_dim] + right[oo_plane_dim]) / 2
+
+    left = left[plane]
+    right = right[plane]
+
+    n_2d = get_2d_normal(left, right)
+
+    result = np.empty((3, ), np.float32)
+    result[plane[0]] = n_2d[0]
+    result[plane[1]] = n_2d[1]
+    result[oo_plane_dim] = oo_mean
+
+    return result
+
+
+@nb.jit(nb.float32[:, :](
+    nb.float32, nb.float32, nb.float32
+), nopython=True, nogil=True)
+def rot3d(a, b, c):
+    """
+    """
+    Rx = np.array([
+        [1., 0., 0.],
+        [0, np.cos(a), -np.sin(a)],
+        [0, np.sin(a), np.cos(a)]
+    ], np.float32)
+    Ry = np.array([
+        [np.cos(b), 0, np.sin(b)],
+        [0., 1., 0.],
+        [-np.sin(b), 0, np.cos(b)]
+    ], np.float32)
+    Rz = np.array([
+        [np.cos(c), -np.sin(c), 0],
+        [np.sin(c), np.cos(c), 0],
+        [0., 0., 1.]
+    ], np.float32)
+    return Rx @ Ry @ Rz
+
+
+@nb.jit(nb.float32[:, :](
+    nb.float32[:, ], nb.float32[:, ]
+), nopython=True, nogil=True)
+def get_3d_rotation_to_align_horizontally(left, right):
+    """ aligns left and right so that both touch the x-axis
+    :param left: (x, y, z)
+    :param right: (x, y, z)
+    :return R
+    """
+    plane = np.array([0, 2], np.int32)
+    x, y, z = get_3d_normal_on_plane(left, right, plane)
+    n = np.array([x, z])
+    alpha = -get_2d_rotation_for_upward(n)
+    R = rot3d(0., alpha, 0.)
+    return R
+
+
+@nb.jit(nb.float32[:, :](
+    nb.float32[:, ], nb.float32[:, ]
+), nopython=True, nogil=True)
+def get_3d_rotation_to_face_forward(left, right):
+    """ align skeleton so that it faces forward in positive
+        y direction (0, 1, 0)
+    :param left: (x, y, z)
+    :param right: (x, y, z)
+    :return R
+    """
+    plane = np.array([0, 1], np.int32)
+    x, y, z = get_3d_normal_on_plane(left, right, plane)
+    n = np.array([x, y])
+    alpha = get_2d_rotation_for_upward(n)
+    R = rot3d(0., 0., alpha)
+    return R
+
+
+def apply_rotation(person, R):
+    """
+    :param person: { J x 3 }
+    :param R: { 3 x 3 }
+    :return:
+    """
+    person_mu = person - np.mean(person, axis=0)
+    person_R = person_mu @ R.T
+    return person_R + np.mean(person, axis=0)
---
+++ mocap/math/test_geometry.py
@@ -0,0 +1,153 @@
+import unittest
+import numpy as np
+import numpy.linalg as la
+import mocap.math.geometry as gm
+from math import pi
+
+
+class Test3DRotation(unittest.TestCase):
+
+    def test_apply_3drot(self):
+        person = np.array([
+            (5, 0, 0),
+            (4, 0, 0),
+            (3, 0, 0),
+            (2, 0, 0),
+            (1, 0, 0)
+        ], np.float32)
+
+        gt_person_rot = np.array([
+            (3,   2, 0),
+            (3,   1, 0),
+            (3,   0, 0),
+            (3,  -1, 0),
+            (3,  -2, 0)
+        ], np.float32)
+
+        R = gm.rot3d(0, 0, pi/2)
+        person_rot = gm.apply_rotation(person, R)
+
+        diff = np.sum(person_rot - gt_person_rot)
+        self.assertAlmostEqual(0, diff)
+
+    def test_3drot(self):
+        a = np.array([1, 0, 0])
+        R = gm.rot3d(0, pi/2, 0)
+        b = R @ a
+
+        self.assertAlmostEqual(b[0], 0)
+        self.assertAlmostEqual(b[1], 0)
+        self.assertAlmostEqual(b[2], -1)
+
+    def test_find_3d_normal_on_plane(self):
+        left = np.array([-1, 0, 0], np.float32)
+        right = np.array([1, 0, 0], np.float32)
+        plane = np.array([0, 1], np.int32)
+
+        n = gm.get_3d_normal_on_plane(left, right, plane)
+
+        self.assertAlmostEqual(n[0], 0)
+        self.assertAlmostEqual(n[1], 1)
+        self.assertAlmostEqual(n[2], 0)
+
+
+class TestDistances(unittest.TestCase):
+
+    def test_distances(self):
+        A = np.array([
+            (1, 1),
+            (2, 2),
+            (3, 3)
+        ], np.float32)
+        B = np.array([
+            (1, 5),
+            (2, 4),
+            (3, 3)
+        ], np.float32)
+
+        d = gm.distances(A, B)
+        self.assertEqual(len(d), 3)
+        self.assertAlmostEqual(d[0], 4)
+        self.assertAlmostEqual(d[1], 2)
+        self.assertAlmostEqual(d[2], 0)
+
+
+class Test2DRotation(unittest.TestCase):
+
+    def test_simple2d_rotation(self):
+        alpha = 0
+        R = gm.rot2d(alpha)
+        self.assertEqual(len(R.shape), 2)
+        self.assertEqual(R.shape[0], 2)
+        self.assertEqual(R.shape[1], 2)
+
+        self.assertAlmostEqual(R[0,0], 1)
+        self.assertAlmostEqual(R[1,1], 1)
+        self.assertAlmostEqual(R[1,0], 0)
+        self.assertAlmostEqual(R[0,1], 0)
+
+    def test_rot90(self):
+        alpha = pi/2
+        R = gm.rot2d(alpha)
+        self.assertEqual(len(R.shape), 2)
+        self.assertEqual(R.shape[0], 2)
+        self.assertEqual(R.shape[1], 2)
+
+        a = np.array([1, 0])
+        b = R @ a
+
+        self.assertAlmostEqual(b[0], 0)
+        self.assertAlmostEqual(b[1], 1)
+
+    def test_rot270(self):
+        alpha = pi + pi/2
+        R = gm.rot2d(alpha)
+        self.assertEqual(len(R.shape), 2)
+        self.assertEqual(R.shape[0], 2)
+        self.assertEqual(R.shape[1], 2)
+
+        a = np.array([1, 0])
+        b = R @ a
+
+        self.assertAlmostEqual(b[0], 0)
+        self.assertAlmostEqual(b[1], -1)
+
+    def test_normal(self):
+        left = np.array([-1, 0], np.float32)
+        right = np.array([1, 0], np.float32)
+
+        n = gm.get_2d_normal(left, right)
+        self.assertEqual(len(n), 2)
+        self.assertAlmostEqual(la.norm(n), 1)
+
+        self.assertAlmostEqual(n[0], 0)
+        self.assertAlmostEqual(n[1], 1)
+
+    def eval_2d_rotation_upward(self, p):
+        p = p.astype(np.float32)
+        alpha = gm.get_2d_rotation_for_upward(p)
+        R = gm.rot2d(alpha)
+        p_norm = p / la.norm(p)
+        up = R @ p_norm
+        self.assertAlmostEqual(up[0], 0, places=4)
+        self.assertAlmostEqual(up[1], 1, places=4)
+
+    def test_2d_rotation_upward(self):
+        p = np.array([1, 0])
+        self.eval_2d_rotation_upward(p)
+
+        p = np.array([1.7, 1.2])
+        self.eval_2d_rotation_upward(p)
+
+        p = np.array([-1.7, 1.2])
+        self.eval_2d_rotation_upward(p)
+
+        p = np.array([1.7, -1.2])
+        self.eval_2d_rotation_upward(p)
+
+        p = np.array([-1.7, -1.2])
+        self.eval_2d_rotation_upward(p)
+
+
+if __name__ == '__main__':
+    unittest.main()
