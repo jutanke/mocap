9003ef764c4a2034c701d15551af1239d6f499f6
Julian Tanke <jutanke@googlemail.com>
2019-04-23 10:06:11 +0200

    add math to package

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name="mocap",
-    version="0.0.3",
+    version="0.0.4",
     packages=["mocap",
               "mocap/data",
               "mocap/visualization",
+              "mocap/math",
               "mocap/processing"
               ],
 )
