33c9b3b58302a31aee31a36547e7586eba21b5bb
Julian Tanke <jutanke@googlemail.com>
2019-07-01 10:31:35 +0200

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="mocap",
-    version="0.0.12",
+    version="0.0.13",
     packages=["mocap",
               "mocap/data",
               "mocap/visualization",
