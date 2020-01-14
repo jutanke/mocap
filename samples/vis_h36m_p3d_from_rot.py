c6dd8eb0bb9aef31cf3c363534e764b4541e05ce
jutanke <jutanke@googlemail.com>
2020-01-14 18:43:25 +0100

    use kabsch to rotate rotational to 3d data

--- samples/vis_h36m_p3d_from_rot.py
+++ samples/vis_h36m_p3d_from_rot.py
@@ -6,6 +6,8 @@ from os import makedirs
 from mocap.visualization.sequence import SequenceVisualizer
 import mocap.processing.normalize as norm
 import mocap.math.fk as FK
+import mocap.math.kabsch as KB
+import  numpy as np
 
 vis_dir = '../output/'
 if not isdir(vis_dir):
@@ -16,5 +18,12 @@ vis = SequenceVisualizer(vis_dir, 'vis_h36m_p3d_from_rot', to_file=True, mark_or
 
 Seq1 = norm.remove_rotation_and_translation(H36M.get3d_fixed_from_rotation('S1', 'walking', 1)[0:250:5])
 Seq2 = norm.remove_rotation_and_translation(H36M.get3d('S1', 'walking', 1)[0:250:5])
+
+Seq1_ = []
+
+for p, q in zip(Seq1, Seq2):
+    p = KB.rotate_P_to_Q(p, q)
+    Seq1_.append(p)
+Seq1 = np.array(Seq1_)
 
 vis.plot(Seq1, seq2=Seq2, parallel=True)
