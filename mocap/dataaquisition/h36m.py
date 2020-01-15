human36m: fixed skeletons now retain global rotation + translation

mocap/dataaquisition/h36m.py
@@ -4,8 +4,11 @@ from os.path import join, isdir, isfile
 from os import makedirs
 import mocap.datasets.h36m as H36M
 import mocap.processing.conversion as conv
+import mocap.processing.normalize as norm
 import numpy as np
 import mocap.settings as settings
+import mocap.math.kabsch as KB
+from tqdm import tqdm
 
 DATA_DIR = join(settings.get_data_path(), 'h36m')
 
@@ -15,11 +18,28 @@ def acquire_fixed_skeleton():
     global DATA_DIR
     data_dir = join(DATA_DIR, 'fixed_skeleton')
     if not isdir(data_dir):
+        print('[mocap][Human3.6M] generate fixed skeletons:', data_dir)
         makedirs(data_dir)
         for actor in H36M.ACTORS:
-            for action in H36M.ACTIONS:
+            print('\thandle actor ', actor)
+            for action in tqdm(H36M.ACTIONS):
                 for sid in [1, 2]:
                     fname = join(data_dir, actor + '_' + action + '_' + str(sid) + '.txt')
+                    # seq1 = norm.remove_rotation_and_translation(
+                    #     H36M.get3d_fixed_from_rotation(actor, action, sid))
+                    # seq2 = norm.remove_rotation_and_translation(
+                    #     H36M.get3d(actor, action, sid))
+                    seq1 = H36M.get3d_fixed_from_rotation(actor, action, sid)
+                    seq2 = H36M.get3d(actor, action, sid)
+                    assert len(seq1) == len(seq2), actor + ' ' + action + ' -> ' + str(seq1.shape) + '|' + str(seq2.shape)
+
+                    seq1_ = []
+                    for p, q in zip(seq1, seq2):
+                        p = KB.rotate_P_to_Q(p, q)
+                        seq1_.append(p)
+                    n_frames = len(seq1)
+                    seq1 = np.reshape(seq1_, (n_frames, -1))
+                    np.savetxt(fname, seq1)
 
 
 def acquire_euler():

mocap/datasets/h36m.py
@@ -20,10 +20,12 @@ for subdir, needs_password in zip(['labels'], [True]):
     zip_files = [f for f in listdir(join(data_dir, subdir)) if f.endswith('.zip')]
     txt_files = [f for f in listdir(join(data_dir, subdir)) if f.endswith('.txt')]
     if len(zip_files) > len(txt_files):
-        print('\n[Human3.6M] decompress data.. ->', subdir)
+        print('\n[mocap][Human3.6M] decompress data.. ->', subdir)
 
         if needs_password:
             if not isfile(password_file):
+                print("\t\n\033[1m\033[93mCannot find password file... skipping decompression\033[0m")
+                print()
                 continue
             assert isfile(password_file), 'could not find ' + password_file + '!!'
             password = open(password_file, 'r').read()
@@ -45,6 +47,7 @@ def get3d(actor, action, sid):
 
 
 def get3d_fixed(actor, action, sid):
+    H36M_DA.acquire_fixed_skeleton()
     fname = join(join(data_dir, 'fixed_skeleton'), actor + '_' + action + '_' + str(sid) + '.txt')
     seq = np.loadtxt(fname, dtype=np.float32)
     return seq