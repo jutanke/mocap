d4e328ca6aa5f4f755969d3e27febd3b59f8ab1a
jutanke <jutanke@googlemail.com>
2020-01-17 16:01:04 +0100

    add label names for h36m

--- mocap/datasets/h36m.py
+++ mocap/datasets/h36m.py
@@ -168,6 +168,19 @@ ACTIONS = [
 
 ACTORS = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
 
+LABEL_NAMES = [
+    'kneeling',         # 0
+    'kneeling down',    # 1
+    'leaning down',     # 2
+    'sitting chair',    # 3
+    'sitting down',     # 4
+    'sitting floor',    # 5
+    'squatting',        # 6
+    'standing',         # 7
+    'standing up',      # 8
+    'steps',            # 9
+    'walking']          # 10
+
 
 class H36M_Simplified(DataSet):
 
