572837879d9557edf1380efeec5604cdc07a20d2
Julian <jutanke@googlemail.com>
2020-03-07 18:55:00 +0100

    version +1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ site_package = 'lib/python' + p1 + '.' + p2 + '/site-packages/mocap'
 
 setup(
     name="mocap",
-    version="1.0.16",
+    version="1.0.17",
     packages=["mocap",
               "mocap/datasets",
               "mocap/visualization",
