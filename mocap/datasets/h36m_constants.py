7c8832bba434b03b548154c3d7c1eb5f48aeaac6
Julian Tanke <jutanke@googlemail.com>
2020-04-30 11:22:23 +0200

    remove circular imports - move to constants file

---
+++ mocap/datasets/h36m_constants.py
@@ -0,0 +1,32 @@
+ACTIONS = [
+    'directions',
+    'discussion',
+    'eating',
+    'greeting',
+    'phoning',
+    'posing',
+    'purchases',
+    'sitting',
+    'sittingdown',
+    'smoking',
+    'takingphoto',
+    'waiting',
+    'walking',
+    'walkingdog',
+    'walkingtogether'
+]
+
+ACTORS = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
+
+LABEL_NAMES = [
+    'kneeling',         # 0
+    'kneeling down',    # 1
+    'leaning down',     # 2
+    'sitting chair',    # 3
+    'sitting down',     # 4
+    'sitting floor',    # 5
+    'squatting',        # 6
+    'standing',         # 7
+    'standing up',      # 8
+    'steps',            # 9
+    'walking']          # 10
