6c1b0077a9e4fd769d596c238ea1456d0428ddba
Julian Tanke <jutanke@googlemail.com>
2020-05-08 23:29:45 +0200

    version ++

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ site_package = 'lib/python' + p1 + '.' + p2 + '/site-packages/mocap'
 
 setup(
     name="mocap",
-    version="1.0.31",
+    version="1.0.32",
     packages=["mocap",
               "mocap/datasets",
               "mocap/visualization",
