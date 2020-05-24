aa081f96d7eb6344d4445c9a71d8ed74c9121a6e
tanke <tanke@iai.uni-bonn.de>
2020-05-24 22:43:41 +0200

    cmu-eval with 3d

--- mocap/datasets/cmu_eval.py
+++ mocap/datasets/cmu_eval.py
@@ -2,10 +2,10 @@ import numba as nb
 import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join, dirname, isdir, isfile, abspath
-from os import listdir
+from os import listdir, makedirs
 from transforms3d.euler import euler2mat
 from mpl_toolkits.mplot3d import Axes3D
-import mocap.dataaquisition.cmu as CMU_DA
+from mocap.math.fk_cmueval import angular2euclidean
 from mocap.datasets.dataset import DataSet
 import mocap.processing.normalize as norm
 from enum import IntEnum
@@ -49,6 +49,52 @@ class CMUEval(DataSet):
             iterate_with_framerate=False,
             iterate_with_keys=False,
             j_root=0, j_left=0, j_right=0,
+            n_joints=38,
+            mirror_fn=None)
+    
+
+
+class CMUEval3D(DataSet):
+
+    def __init__(self, activities, datatype, data_storage_dir='/tmp'):
+        local_data_dir = abspath(join(dirname(__file__), '../data/cmu_eval'))
+        data_storage_dir = join(data_storage_dir, 'cmueval')
+        if datatype == DataType.TEST:
+            data_storage_dir = join(data_storage_dir, 'test')
+            local_data_dir = join(local_data_dir, 'test')
+        else:
+            data_storage_dir = join(data_storage_dir, 'train')
+            local_data_dir = join(local_data_dir, 'train')
+
+        seqs = []
+        keys = []
+        for activity in activities:
+            loc = join(local_data_dir, activity)
+            assert isdir(loc), loc
+
+            loc_npy = join(data_storage_dir, activity)
+            if not isdir(loc_npy):
+                makedirs(loc_npy)
+
+            files = sorted([f for f in listdir(loc) if f.endswith('.txt')])
+            for f in files:
+                f_npy = join(loc_npy, f) + '.npy'
+                fname = join(loc, f)
+                if isfile(f_npy):
+                    seq = np.load(f_npy)
+                else:
+                    seq = np.loadtxt(fname, delimiter=',').astype('float32')
+                    seq = angular2euclidean(seq).astype('float32')
+                    np.save(f_npy, seq)
+                keys.append(fname)
+                seqs.append(seq.astype('float32'))
+        
+        super().__init__(
+            [seqs], Keys=keys,
+            framerate=120,
+            iterate_with_framerate=False,
+            iterate_with_keys=False,
+            j_root=-1, j_left=8, j_right=2,
             n_joints=38,
             mirror_fn=None)
 
--- samples/vis_cmu_eval.py
+++ samples/vis_cmu_eval.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, './..')
 from os.path import isdir
 from os import makedirs
-from mocap.datasets.cmu_eval import CMUEval, ACTIVITES, DataType
+from mocap.datasets.cmu_eval import CMUEval, CMUEval3D, ACTIVITES, DataType
 from mocap.visualization.sequence import SequenceVisualizer
 import mocap.processing.normalize as norm
 import mocap.math.fk_cmueval as FK
@@ -14,10 +14,13 @@ vis_dir = '../output/'
 if not isdir(vis_dir):
     makedirs(vis_dir)
 
-ds = CMUEval(ACTIVITES, DataType.TEST)
+ds = CMUEval3D(ACTIVITES, DataType.TEST)
 
 seq = ds[0][:50]
 print('seq', seq.shape)
+
+
+exit(1)
 
 seq3d = FK.angular2euclidean(seq)
 seq3d = norm.remove_rotation_and_translation(
